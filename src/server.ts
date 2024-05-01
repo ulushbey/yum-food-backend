273610d645c26b744ed5111aa81613e941e63fb8
ulushbey <ulushbey.h@gmail.com>
2024-05-01 17:47:12 +0900

    feat: creating  MongoDB connection with PORT

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello Food Project");
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succesfull");
+    const PORT = process.env.PORT ?? 3006;
+  })
+  .catch((err) => console.log("Error on connection MongoDB", err));
