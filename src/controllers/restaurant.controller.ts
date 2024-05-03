11d65009f4e50a1e7da7d2ea43efe4d3d2c5f32b
ulushbey <ulushbey.h@gmail.com>
2024-05-04 02:50:11 +0900

    feat: develop frontend admin page

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -64,7 +64,7 @@ restaurantController.processSignup = async (
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert ("${message}"); window.location.replace('admin/signup') </script>`
+      `<script> alert ("${message}"); window.location.replace('/admin/signup') </script>`
     );
   }
 };
@@ -89,7 +89,7 @@ restaurantController.processLogin = async (
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert ("${message}"); window.location.replace('admin/login') </script>`
+      `<script> alert ("${message}"); window.location.replace('/admin/login') </script>`
     );
   }
 };
--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,55 @@
 console.log("Signup frontend javascript file");
+
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpg, jpeg and png file!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
+
+function validateSignupForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please insert all required inputs!");
+    return false;
+  }
+  if (memberPassword !== confirmPassword) {
+    alert("Password differs, please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get[0].files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : "null";
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
+    return false;
+  }
+}
