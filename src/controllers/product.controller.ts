fc92d641b4ea8ab0d9593e968b48130811a05230
ulushbey <ulushbey.h@gmail.com>
2024-05-04 19:16:26 +0900

    feat: modify product page BSSR

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -36,19 +36,19 @@ productController.createNewProduct = async (
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
     const data: ProductInput = req.body;
     data.productImages = req.files?.map((ele) => {
-      return ele.path;
+      return ele.path.replace(/\\/g, "/");
     });
 
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert ("Successful creation!"); window.location.replace('admin/product/all') </script>`
+      `<script> alert ("Successful creation!"); window.location.replace('/admin/product/all') </script>`
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert ("${message}"); window.location.replace('admin/product/all') </script>`
+      `<script> alert ("${message}"); window.location.replace('/admin/product/all') </script>`
     );
   }
 };
