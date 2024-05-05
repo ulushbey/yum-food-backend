73ed9db0f5ceea760f9bd948832393ddc3d6986c
ulushbey <ulushbey.h@gmail.com>
2024-05-05 19:02:44 +0900

    feat: develop getUpdate member business logic

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ const store = new MongoDBStore({
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("./uploads"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -6,6 +6,7 @@ import {
   LoginInput,
   Member,
   MemberInput,
+  MemberUpdateInput,
 } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
@@ -75,6 +76,20 @@ memberController.getMemberDetail = async (
   try {
     console.log("getMemberDetail");
     const result = await memberService.getMemberDetail(req.member);
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, logout:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateMember");
+    const input: MemberUpdateInput = req.body;
+    if (req.file) input.memberImage = req.file.path.replace(/\\/, "/");
+    const result = await memberService.updateMember(req.member, input);
 
     res.status(HttpCode.OK).json(result);
   } catch (err) {
