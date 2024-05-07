ad3230f883436c264c7bbe41855799f0d8e9b6c5
ulushbey <ulushbey.h@gmail.com>
2024-05-07 23:56:17 +0900

    feat: corrected home.js file

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,98 +1,81 @@
-console.log("Home frontend javascript file");
+console.log("Hello world! ");
 
 function fitElementToParent(el, padding) {
-  let timeout = null;
-
+  var timeout = null;
   function resize() {
     if (timeout) clearTimeout(timeout);
     anime.set(el, { scale: 1 });
-    let pad = padding || 0,
-      parentEl = el.parentNode,
-      elOffsetWidth = el.offsetWidth - pad,
-      parentOffsetWidth = parentEl.offsetWidth,
-      ratio = parentOffsetWidth / elOffsetWidth;
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
     timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
   }
-
   resize();
   window.addEventListener("resize", resize);
 }
 
-(function () {
-  const sphereEl = document.querySelector(".sphere-animation"),
-    spherePathEls = sphereEl.querySelectorAll(".sphere path"),
-    pathLength = spherePathEls.length,
-    animations = [];
+var easingsAnimation = (function () {
+  var easingVisualizerEl = document.querySelector(".easing-visualizer");
+  var barsWrapperEl = easingVisualizerEl.querySelector(".bars-wrapper");
+  var dotsWrapperEl = easingVisualizerEl.querySelector(".dots-wrapper");
+  var barsFragment = document.createDocumentFragment();
+  var dotsFragment = document.createDocumentFragment();
+  var numberOfBars = 91;
+  var duration = 450;
+  var animation;
 
-  fitElementToParent(sphereEl);
+  fitElementToParent(easingVisualizerEl, 0);
 
-  const breathAnimation = anime({
-    begin: function () {
-      for (let i = 0; i < pathLength; i++) {
-        animations.push(
-          anime({
-            targets: spherePathEls[i],
-            stroke: {
-              value: ["rgba(255,75,75,1)", "rgba(80,80,80,.35)"],
-              duration: 500,
-            },
-            translateX: [2, -4],
-            translateY: [2, -4],
-            easing: "easeOutQuad",
-            autoplay: false,
-          })
-        );
-      }
-    },
-    update: function (ins) {
-      animations.forEach(function (animation, i) {
-        let percent = (1 - Math.sin(i * 0.35 + 0.0022 * ins.currentTime)) / 2;
-        animation.seek(animation.duration * percent);
-      });
-    },
-    duration: Infinity,
-    autoplay: false,
-  });
-
-  const introAnimation = anime
-    .timeline({
-      autoplay: false,
-    })
-    .add(
-      {
-        targets: spherePathEls,
-        strokeDashoffset: {
-          value: [anime.setDashoffset, 0],
-          duration: 3900,
-          easing: "easeInOutCirc",
-          delay: anime.stagger(190, { direction: "reverse" }),
-        },
-        duration: 2000,
-        delay: anime.stagger(60, { direction: "reverse" }),
-        easing: "linear",
-      },
-      0
-    );
-
-  const shadowAnimation = anime(
-    {
-      targets: "#sphereGradient",
-      x1: "25%",
-      x2: "25%",
-      y1: "0%",
-      y2: "75%",
-      duration: 30000,
-      easing: "easeOutQuint",
-      autoplay: false,
-    },
-    0
-  );
-
-  function init() {
-    introAnimation.play();
-    breathAnimation.play();
-    shadowAnimation.play();
+  for (var i = 0; i < numberOfBars; i++) {
+    var barEl = document.createElement("div");
+    var dotEl = document.createElement("div");
+    barEl.classList.add("bar");
+    dotEl.classList.add("dot");
+    dotEl.classList.add("color-red");
+    barsFragment.appendChild(barEl);
+    dotsFragment.appendChild(dotEl);
   }
 
-  init();
+  barsWrapperEl.appendChild(barsFragment);
+  dotsWrapperEl.appendChild(dotsFragment);
+
+  function play() {
+    var easings = [];
+    for (let ease in anime.penner) easings.push(ease);
+    easings.push("steps(" + anime.random(5, 20) + ")");
+    easings.push("steps(" + anime.random(5, 20) + ")");
+    easings.push("cubicBezier(0.545, 0.475, 0.145, 1)");
+    var ease = easings[anime.random(0, easings.length - 1)];
+
+    animation = anime
+      .timeline({
+        duration: duration,
+        easing: ease,
+        complete: play,
+      })
+      .add({
+        targets: ".easing-visualizer .bar",
+        scaleY: anime.stagger([1, 44], {
+          easing: ease,
+          from: "center",
+          direction: "reverse",
+        }),
+        delay: anime.stagger(7, { from: "center" }),
+      })
+      .add(
+        {
+          targets: ".easing-visualizer .dot",
+          translateY: anime.stagger(["-160px", "160px"], {
+            easing: ease,
+            from: "last",
+          }),
+          delay: anime.stagger(7, { from: "center" }),
+        },
+        0
+      );
+  }
+
+  play();
 })();
